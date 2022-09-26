7b9f04da77b34cbd8204a964029eaa66789482da
LayalChristine <layal.lettry@gmail.com>
2022-09-26 16:23:54 +0200

    Add some paths

--- inst/run_prepare_input_llc.R
+++ inst/run_prepare_input_llc.R
@@ -3,9 +3,12 @@
 ## Load package ----------------------------------------------------------------
 
 # Load from repository on server:
-libloc <- "O:/MASS/04_software/01_r/lib_research/lib_rrclust"
-.libPaths(c(.libPaths(), libloc))
-library(delfin, lib.loc = libloc)
+# libloc <- "O:/MASS/04_software/01_r/lib_research/lib_rrclust"
+# .libPaths(c(.libPaths(), libloc))
+# library(delfin, lib.loc = libloc)
+
+# Local
+library(rrclust)
 
 # For local test or development purposes load sources virtually:
 # devtools::load_all(".")
@@ -15,8 +18,10 @@ options(browserNLdisabled = TRUE)
 # libloc <- "/Users/donzel/Développement/OFAS/package_tools/softwares/lib_rrclust"
 
 
-root <- "O:/MASS/09_mathprod/01_fh/container_tools/inputs/research"
+# root <- "O:/MASS/09_mathprod/01_fh/container_tools/inputs/research"
 # root <- "C:/research/inputs/research"
+root <- "/Users/Layal/OFAS/doctorat/package_tools/data"
+
 # inp <- "inp_kamila"
 inp <- "inp_kamila_large"
 

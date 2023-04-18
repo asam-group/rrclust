76e505b7d196eb57cba03787d0b909e0768fa892
Layal Christine Lettry <layal@cynkra.com>
2023-04-18 20:25:02 +0200

    fix: remove 'Hmisc::describe' and replace with describe

--- R/descr_stat_fun.R
+++ R/descr_stat_fun.R
@@ -39,7 +39,7 @@ descr_stat_fun <- function(descrstat_dpath, lvalues) {
 
     # --- Function describe for the histogram ----------------------------------
 
-    des.x <- Hmisc::describe(x, descript = name_x)
+    des.x <- describe(x, descript = name_x)
     tab1l <- latex(des.x, file = file_hist_tex)
 
     # --- Definition of ad hoc functions ---------------------------------------
